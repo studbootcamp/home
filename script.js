c70c8ecb05ed9b7e7238287520ef80b6d8d01d99
studbootcamp <irenix.code@gmail.com>
2024-09-06 17:21:49 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let loaader = document.getElementById("loader")
+function initaos(){
+    AOS.init({
+        duration:400,
+        easing:"ease-in-out",
+        once:true,
+        mirror:false
+    })
+}
+window.addEventListener('load',initaos)
+
+function removeLoader(){
+    loaader.remove()
+}
+window.addEventListener('load',removeLoader)
+
+
+let studentData = [
+    {name:"Abubakar Muhammed Abubakar",repo:"abubakar"},
+    {name:"Asma'u Abdullahi Umar",repo:"asamu"},
+    {name:"Fatima Abdulrahman Aliyu",repo:"fatima"},
+    {name:"Judah Anofukei Mamiso",repo:"judah"},
+    {name:"Mus'ab Adamu Kawukano",repo:"musab"},
+    {name:"Nusaybah Umar Faruk",repo:"nusaiba"},
+    {name:"Safiyyah Adamu Kawukano",repo:"safiyyah"},
+    {name:"Samuel Joseph",repo:"samuel"},
+    {name:"Umar Faruq Abdullahi",repo:"umar"},
+    {name:"Usman Abulrrahamn Aliyu",repo:"usman"},
+]
+
+let student = document.getElementById("students")
+
+studentData.map(x=>{
+    student.innerHTML += `
+         <a href="https://studbootcamp.github.io/${x.repo}" data-aos="from-left" class="stud bg-light text-dark shadow m-2 col-lg-3 col-sm-5">
+                    <h4>${x.name}</h4>
+                </a>
+        
+    `
+})
